f6fbc3237f14efb9db2cb9cddeda3df795c315f3
Evgenii <skayl8827@gmail.com~>
2024-08-05 19:55:26 +0700

    init

---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify, url_for, send_file
+import edge_tts
+import os
+import asyncio
+
+app = Flask(__name__)
+
+@app.route('/text-to-speech', methods=['POST'])
+def text_to_speech():
+    data = request.json
+    text = data.get('text')
+    filename = data.get('filename')
+    model = data.get('model', 'en-US-GuyNeural')  # используем модель по умолчанию, если не указана
+
+    if not text or not filename:
+        return jsonify({"error": "Please provide both text and filename"}), 400
+
+    try:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(generate_audio(text, filename, model))
+        loop.close()
+
+        # Путь к аудио файлу
+        audio_path = url_for('static', filename=f'{filename}.mp3', _external=True)
+        return jsonify({"audio_url": audio_path})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+async def generate_audio(text, filename, model):
+    communicate = edge_tts.Communicate(text, model)
+    audio_path = os.path.join('static', f"{filename}.mp3")
+    await communicate.save(audio_path)
+
+if __name__ == '__main__':
+    if not os.path.exists('static'):
+        os.makedirs('static')
+    app.run(host='0.0.0.0', port=5000)
